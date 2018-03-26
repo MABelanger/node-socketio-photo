8ef529cffea742a4239fd54ec0e19dd69b87a725
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2018-03-26 14:57:25 -0400

    split saveImage

--- src/lib/components/photos/photosController.js
+++ src/lib/components/photos/photosController.js
@@ -2,31 +2,33 @@
 
 const db = require('./db');
 
+function _send400Err (res, reject, reason) {
+  let err = {
+    msg: 'saveImage error! ' + reason
+  };
+  res.status(400).json(err);
+  reject(err);
+}
+
+function _saveDb (dataUri, res, resolve, reject) {
+  // console.log(req.connection.remoteAddress)
+  db.saveImage(dataUri)
+    .then((imageInfo) => {
+      res.json(imageInfo);
+      resolve(imageInfo);
+    })
+    .catch(function (reason) {
+      _send400Err(res, reject, reason);
+    });
+}
+
 function saveImage (req, res) {
   let promiseCtrl = new Promise((resolve, reject) => {
     if (req.body && req.body.dataUri) {
-      let promiseDb = db.saveImage(req.body.dataUri);
-
-      // console.log(req.connection.remoteAddress)
-      promiseDb
-        .then((imageInfo) => {
-          res.json(imageInfo);
-          resolve(imageInfo);
-        })
-        .catch(function (reason) {
-          console.log('rejection promiseDb');
-          let err = {
-            msg: 'saveImage error! ' + reason
-          };
-          res.status(400).json(err);
-          reject(err);
-        });
+      let dataUri = req.body.dataUri;
+      _saveDb(dataUri, res, resolve, reject);
     } else {
-      let err = {
-        msg: 'saveImage error! ' + 'No dataUri'
-      };
-      res.status(400).json(err);
-      reject(err);
+      _send400Err(res, reject, 'dataUri missing');
     }
   });
 
